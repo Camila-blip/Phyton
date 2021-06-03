3accf4b291fc030c459e7e7e53b3d0aa6d5808ab
Camila-blip <59473635+Camila-blip@users.noreply.github.com>
2021-06-02 23:26:59 -0300

    Create in.py

---
+++ in.py
@@ -0,0 +1,11 @@
+#in é se existe em determinada variavel
+x = 5 in (1,2,3,4,5)
+#cria uma lista
+y = 2 in range(1,6)
+#terceiro numero e iguala  quantidade de casas que ele ira pular
+x = 2 in range(2,11,2)
+#ordem decrescente
+z = 2 in range(11,2,-2)
+
+w = (2 or 10) and (3 or 11) in (1,2,3,4,5)
+print(w)
